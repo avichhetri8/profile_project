22af83ab6eeb4f6cc70153a63dafaefd1f7d57ba
Avi <avichhetri8@gmail.com>
2020-12-09 16:45:40 +0545

    user model created

---
+++ profile_app/models.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+
+    def create_user(self, email, name, password=None):
+
+        if not email:
+            raise ValueError("User must have a email address")
+
+        email = self.normalize_email(email)
+        user = self.model(name=name , email= email)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email,name,password)
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save(using=self._db)
+        return user
+
+
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=80, unique=True)
+    name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
